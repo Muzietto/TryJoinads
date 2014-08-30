9e55743b43e3753aa00a3a216538e46a7b04f77b
Muzietto <marco_faustinelli@yahoo.com>
2014-08-30 16:38:40 +0200

    Still fighting imperative monad

--- state/state3.fs
+++ state/state3.fs
@@ -8,13 +8,17 @@ let (>>=) sa fasb = bindState sa fasb
 
 let returnState a = fun s -> (s,a)
 
+let getState = fun s -> (s,s)
+let setState ss = fun _ -> (ss,())
+
 type state<'s,'a> = 's -> 's * 'a
 
 type StateClass() = 
-    member kkk.Bind sa fasb = bindState sa fasb
-    member kkk.Return a = returnState a
+    member this.Bind(sa: state<'s,'a>, fasb: 'a -> state<'s,'b>): state<'s,'b> = bindState sa fasb
+    member this.Return(a: 'a): state<'s,'a> = returnState a
+    member this.Zero() = returnState ()
     
-let state = new StateClass();;
+let state = StateClass();;
 
 // type Stack = int list // this is just an alias!
 
@@ -22,26 +26,36 @@ type Stack = StackContents of int list // good idea!
 
 let stacco1 = StackContents [1;2;3;4;5]
 
-// push(x) is a state monad
-let push x s = state {
+// stack -> (cdr stack,car stack)
+let statePop:state<Stack,int> = fun s -> state { 
     match s with
-    | StackContents items -> 
-        return (StackContents (x::items),())
+//    | StackContents [] -> return 0
+    | StackContents (head::tail) ->
+        
+       // do! setState(StackContents tail)
+       // return head
+
+        setState(StackContents tail) >>= (fun _ -> state.Return head)
 }
 
-// stack -> (cdr stack,car stack)
-let statePop s = state {
+// push(x) is a state monad
+let statePush(x:int):state<Stack,int> = state { fun s ->
     match s with
-    | StackContents [] -> 
-        return (StackContents [],0)
-    | StackContents (head::tail) -> 
-        return (StackContents tail,head)
+ //   | StackContents [] -> return 0
+    | StackContents items -> 
+    
+       // do! setState(StackContents (x::items))
+       // return ()
+        
+         setState(StackContents (x::items)) >>= (fun _ -> state.Return ())
 }
 
 // imperative monad
 
-let im1 = statePop >>= (fun x -> returnState x)
+let qweqwe = statePop StackContents [1,2,3,4,5] 123
+
+//let im1 = statePop >>= (fun x -> returnState x)
+//let wer = im1 [5,4,3]
 
 //let impMonad = statePop >>= (fun x -> statePop >>= (fun y -> statePush x))
 
-let wer = im1 [5,4,3]
